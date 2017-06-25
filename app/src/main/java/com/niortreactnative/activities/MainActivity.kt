cc1a5c77fce5ef4f86923916ab1d798076c42a3e
davidc <dcz.switcher@gmail.com>
2017-06-25 16:28:37 +0200

    move MainActivity in activities folder

--- app/src/main/java/com/niortreactnative/activities/MainActivity.kt
+++ app/src/main/java/com/niortreactnative/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.niortreactnative
+package com.niortreactnative.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.niortreactnative.R
 
 class MainActivity : AppCompatActivity() {
 
