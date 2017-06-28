a14c69a8c9b88a894c819c12cadf689953972875
davidc <dcz.switcher@gmail.com>
2017-06-28 22:47:25 +0200

    ajout bouton + pose écouteur

--- app/src/main/java/com/niortreactnative/fragments/LineListFragment.kt
+++ app/src/main/java/com/niortreactnative/fragments/LineListFragment.kt
@@ -1,14 +1,17 @@
 package com.niortreactnative.fragments
 
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 import com.niortreactnative.R
+import kotlinx.android.synthetic.main.fragment_line_list.*
 
 
 /**
@@ -18,14 +21,29 @@ class LineListFragment : Fragment() {
 
     val TAG:String = "LineListFragment"
 
+    var _view:View? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        Log.d(TAG, "oncreate")
+        _view = inflater!!.inflate(R.layout.fragment_line_list, container, false)
 
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_line_list, container, false)
+        return _view
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        addListeners()
+    }
+
+
+    fun addListeners() {
+        this.line_1.setOnClickListener ({view ->
+            Log.d(TAG, ": click :-)")
+        })
+    }
+
+
 
 }// Required empty public constructor
