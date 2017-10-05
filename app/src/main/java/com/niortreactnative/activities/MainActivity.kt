349a7ccbe5a1ff06c909f5725653cdb1ce6582b9
davidc <dcz.switcher@gmail.com>
2017-10-05 20:01:17 +0200

    create line detail fragment and show it by default

--- app/src/main/java/com/niortreactnative/activities/MainActivity.kt
+++ app/src/main/java/com/niortreactnative/activities/MainActivity.kt
@@ -3,11 +3,13 @@ package com.niortreactnative.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.niortreactnative.R
+import com.niortreactnative.fragments.LineDetailFragment
 import com.niortreactnative.fragments.LineListFragment
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG_LINELIST_FRAGMENT = "tag_linelist_fragment"
+    private val TAG_LINEDETAIL_FRAGMENT = "tag_linedetail_fragment"
 
     private var lineListFragment:LineListFragment? = null
 
@@ -25,5 +27,10 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.fragment_stack, lineListFragment, TAG_LINELIST_FRAGMENT)
                     .commit()
         }
+
+        //todo manager navigation between views
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_stack, LineDetailFragment(), TAG_LINEDETAIL_FRAGMENT)
+                .commit()
     }
 }
